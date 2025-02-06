da366881779be799cd5eca1d6c26667e350f67e9
opossalite <werbird10@gmail.com>
2025-02-06 11:08:52 -0800

    Adding Token enum

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,12 +1,66 @@
+use std::collections::{HashMap, HashSet};
 
 
+pub enum Token {
+    Parentheses(Vec<Token>),
+    Brackets(Vec<Token>),
+    Braces(Vec<Token>),
+    String(String),
+    Variable(String),
+
+    Bool(bool),
+    Char(char),
+
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    Neq,
+    Lt,
+    Gt,
+    Lte,
+    Gte,
+    Not,
+
+    Equal,
+
+    Let,
+    Var,
+    Colon,
+    Comma,
+    Dollar,
+    Underscore,
+
+
+}
 
 
 
 pub fn tokenize(input: &Vec<char>) -> Vec<Vec<char>> {
+    let weakly_paired = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+    ]);
+    let strongly_paired = HashSet::from([
+        "'''", //multiline comment
+        "\"\"\"", //multiline string
+        "'", //char
+        "\"", //string
+    ]);
+
+
     let ret = Vec::new();
     ret
 }
+
+
+
+
+
+
 
 
 
